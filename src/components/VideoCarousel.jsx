444f6f50758839e19b237a0f9cee877a98b9b267
loyoenock <loyoenock@gmail.com>
2024-08-01 10:25:12 +0300

    Video - progress track

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -52,14 +52,24 @@ const VideoCarousel = () => {
   const handleLoadedMetaData = (i, e) => setLoadedData((prev) => [...prev, e])
 
   useEffect(() => {
-    const currentProgress = 0;
+    let currentProgress = 0;
     let span = videoSpanRef.current;
 
     if (span[videoId]) {
       // animate the progress of the video
       let anim = gsap.to(span[videoId], {
         onUpdate: () => {
+          const progress = Math.ceil(anim.progress() * 100);
 
+          if(progress !=currentProgress) {
+            currentProgress = progress;
+
+            gsap.to(videoDivRef.current[videoId], {
+              width: window.innerWidth < 760 ? '10vw'
+              : window.innerWidth < 1200 ? '10vw'
+              : '4vw'
+            })
+          }
         },
         onComplete: () => {}
       })
