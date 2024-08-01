19889de6e7f11d95fcbaece6c2ece811f926922d
loyoenock <loyoenock@gmail.com>
2024-08-01 10:13:58 +0300

    Section - video component implemented

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,6 +1,7 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { rightImg, watchImg } from "../utils";
+import VideoCarousel from "./VideoCarousel";
 
 const Highlights = () => {
 
@@ -21,6 +22,7 @@ const Highlights = () => {
             <img src={rightImg} alt="right" className="ml-2" /></p> 
           </div>
         </div>
+        <VideoCarousel />
       </div>
     </section>
   );
